1953184c22d7949536a6ba0111e2d9f7fef391f8
siva209 <sivanarayana209@gmail.com>
2021-07-20 12:22:31 +0530

    Added:Create User Model class

---
+++ src/main/java/com/bridgelabz/userregistration/model/UserEntity.java
@@ -0,0 +1,32 @@
+package com.bridgelabz.userregistration.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+@Data
+@Entity
+@Table(name="user_registration")
+public class UserEntity  {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userid;
+	private String FirstName;
+	private String LastName;
+	private String DataOfBirth;
+	private String Email;
+	private String Password;
+	private LocalDateTime RegisteredDate;
+	private LocalDateTime UpdatedDate;
+	@Column(name ="is_verify_email ", columnDefinition = "boolean default false")
+	private boolean verifyEmail;
+	
+}
+
+
